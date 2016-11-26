fc9c397bb9573646d75d89ae304af5b597fe6e52
waxdoll23 <jmcgnty23@gmail.com>
2016-11-26 12:41:14 -0500

    IntelliJ push check

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@
  */
 public class Main {
     public static void main(String[] args){
-        Person bob = new Person(7,2);
+        Person bob = new Person(8,9);
         Display.printInt(bob.getAge());
         bob.setAge(44);
         Display.printInt(bob.getAge());
